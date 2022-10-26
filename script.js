f4480bc8e7204bc4a7c4b27e1ec9c198848b32db
ouissal <ouissal.derraz@gmail.com>
2022-10-26 21:17:20 +0100

    updates

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ sr.reveal(
 sr.reveal(`.project-picture`, {
   origin: "bottom",
 });
-// ***************media queryies µ****************
+// ***************media queries ***************
 // header
 const close = document.querySelector(".close");
 const btnToggle = document.querySelector(".dropleft .btn-toggler");
@@ -42,3 +42,33 @@ btnToggle.addEventListener("click", () => {
     elemen.classList.toggle("none");
   });
 });
+//==========================download resume =================================
+
+console.log("hhh");
+function DownloadFile(fileName) {
+  var url = fileName;
+  //Create XMLHTTP Request.
+  var req = new XMLHttpRequest();
+  req.open("GET", url, true);
+  req.responseType = "blob";
+  req.onload = function () {
+    //Convert the Byte Data to BLOB object.
+    var blob = new Blob([req.response], { type: "application/octetstream" });
+
+    //Check the Browser type and download the File.
+    var isIE = false || !!document.documentMode;
+    if (isIE) {
+      window.navigator.msSaveBlob(blob, fileName);
+    } else {
+      var url = window.URL || window.webkitURL;
+      link = url.createObjectURL(blob);
+      var a = document.createElement("a");
+      a.setAttribute("download", fileName);
+      a.setAttribute("href", link);
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  };
+  req.send();
+}
